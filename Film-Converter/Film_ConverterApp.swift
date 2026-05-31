be46b6e604fbaa677d5f16e17099f77cff7fa87b
peijiezhong <brianzhong9977@gmail.com>
2026-05-31 19:00:24 +0100

    Initial Commit

---
+++ Film-Converter/Film_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_ConverterApp.swift
+//  Film-Converter
+//
+//  Created by Peijie Zhong on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Film_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
